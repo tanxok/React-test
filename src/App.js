1432b49b3dfbb2973d4f015c44babc2d09968cf0
tanxok <limbualex2@gmail.com>
2023-03-31 10:16:43 +0545

    initialize

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./my/home";
+import Renting from "./my/renting";
+import Payment from "./my/payment";
+import Contact from "./my/contact";
+import Login from "./my/Login";
+// import Main from './my/main';
+
+function App() {
+  return (
+    <div>
+    <Router>
+      <Routes>
+            <Route path="/" component={<Renting />} />
+            <Route path="/renting" component={Renting} />
+            <Route path="/payment" component={Payment} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/login" component={Login} />
+          </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
